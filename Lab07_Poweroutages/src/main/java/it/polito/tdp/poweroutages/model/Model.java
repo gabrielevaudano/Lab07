68166b6d643f0f79b4179e5cd11b462ff4887146
Gabri <Gabri@DESKTOP-USI8PSF>
2020-04-28 14:45:05 +0200

    Update terzo commit

--- Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/model/Model.java
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/model/Model.java
@@ -1,6 +1,5 @@
 package it.polito.tdp.poweroutages.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import it.polito.tdp.poweroutages.DAO.*;
@@ -10,21 +9,10 @@ public class Model {
 	private PowerOutageDAO podao;
 	private NercMap map;
 
-	private Integer maxHours;
-	private Integer maxYears;
-	private Integer maxCustomers;
-	
-	private Integer startingYear;
-
-	private List<PowerOutage> powerOutagesFinale;
-	private List<PowerOutage> powerOutages;
-
 	
 	public Model() {
 		podao = new PowerOutageDAO();	
 		map = new NercMap();
-
-		maxCustomers = 0;
 	}
 	
 	public List<Nerc> getNercList() {
